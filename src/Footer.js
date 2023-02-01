1c9493640cdf84f1acf43da40f63ac45d928b59f
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-02-01 11:41:27 -0500

    update
    Added custom favicon to site. Made minor changes in different components.

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <section id="footer">
     <div className="flex align-middle p-4"> 
-      <p className="mx-auto text-sm lg:text-lg text-center font-light">Site built by Nicholas Webb. Source code <a href="https://github.com/nwebb123/personal-portfolio-react" className="text-sky-500">here</a>.</p>
+      <p className="mx-auto text-sm lg:text-lg text-center font-light">Site built by Nicholas Webb. Source code <a href="https://github.com/nwebb123/personal-portfolio-react" target="_blank" rel="noreferrer" className="text-sky-500">here</a>.</p>
     </div>
   </section>
   )
