db9ca6f67d03c20978b2540bc51752af2f32b7ae
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-02-01 22:36:40 -0500

    update
    Fixed Home nav button to target top of page since href="#" is flagged as a warning.

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <nav className="sticky top-0 z-50 py-5 px-12  flex space-x-12  shadow-xl bg-white ">
-          <a href="#">
+          <a href="#top">
             <h1 className="text-xl font-medium">Home</h1>
           </a>
           <a href="#about">
