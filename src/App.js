bfbf2faef55efd6b7887a50382aff825edcae786
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-02-08 21:10:42 -0500

    Styling updates
    styling updates across various components

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Footer from './Footer'
 function App() {
   return (
     <>
-      <div id="top"></div>
+      <div id="home"></div>
       <Navbar />
       <Hero />
       <About />
--- src/Hero.js
+++ src/Hero.js
@@ -4,11 +4,11 @@ import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 function Hero() {
   return (
     <section id="hero" className="min-h-screen hero-bg">
-      <div className="text-center sm:pt-24 md:pt-28">
-        <h2 className="text-5xl md:text-7xl p-4 text-black font-light">
+      <div className="text-center sm:pt-24 md:pt-28 lg:pt-32">
+        <h2 className="text-5xl md:text-7xl xl:text-8xl p-4 text-black font-light">
           Nicholas Webb
         </h2>
-        <h3 className="text-black text-2xl md:text-4xl py-4 md:py-6 font-light">
+        <h3 className="text-black text-2xl md:text-4xl xl:text-5xl py-4 md:py-6 lg:py-8 font-light">
           Web Developer
         </h3>
         {/* <p className="text-lg md:text-2xl p-5 leading-8 md:leading-10 text-black max-w-2xl mx-auto">
